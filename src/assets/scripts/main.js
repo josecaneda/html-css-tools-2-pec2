e944c27aeec60bdfa9d85da2f28308931c928e26
jose caneda <jcaneda@uoc.edu>
2022-12-05 23:53:15 +0100

    p04: countdown does not work

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,7 +5,9 @@
 
 
 import * as bootstrap from 'bootstrap';
-import countdown from 'countdown';
+//import countdown from 'countdown';
+import Countdown  from 'ds-countdown';
+
 
 
 
@@ -16,27 +18,39 @@ import countdown from 'countdown';
 
 
 window.onload = function() {
-    const nextConcertDate = new Date(2023, 0, 8, 19, 0);
-    const nextConcertSite = ''
-    const concertCountdown = document.getElementById('concert-countdown');
+    // const nextConcertDate = new Date(2023, 0, 8, 19, 0);
+    // const nextConcertSite = ''
+    // const concertCountdown = document.getElementById('concert-countdown');
     
-    setInterval( () => {
-        // concertCountdown.textContent = countdown(nextConcertDate)
-        concertCountdown.textContent = countdown( new Date(2000, 0, 1) )
-            .toString()
-            .replace('months', 'meses')
-            .replace('month', 'mes')
-            .replace('days', 'días')
-            .replace('day', 'día')
-            .replace('hours', 'horas')
-            .replace('hour', 'hora')
-            .replace('minutes', 'minutos')
-            .replace('minute', 'minute')
-            .replace('seconds', 'segundos')
-            .replace('second', 'segundo')
-            .replace('and', 'y');
-    }, 1000);
+    // setInterval( () => {
+    //     concertCountdown.textContent = countdown(nextConcertDate)
+    //         .toString()
+    //         .replace('months', 'meses')
+    //         .replace('month', 'mes')
+    //         .replace('days', 'días')
+    //         .replace('day', 'día')
+    //         .replace('hours', 'horas')
+    //         .replace('hour', 'hora')
+    //         .replace('minutes', 'minutos')
+    //         .replace('minute', 'minute')
+    //         .replace('seconds', 'segundos')
+    //         .replace('second', 'segundo')
+    //         .replace('and', 'y');
+    // }, 1000);
 
+    new Countdown({
+        id: 'concert-countdown',
+        // targetTime: '2023-01-08 19:00:00',
+        targetTime: '2022-12-06 19:00:00',
+        noDay: false,
+        hideDayAtZero: false,
+        separator: '/',
+        afterEnd() {
+          // alert("Time over !")
+        }
+      });
+
+    
 
     
     
