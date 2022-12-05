72703df1dafbb4ed383aba7ca91902d3f45b8550
jose caneda <jcaneda@uoc.edu>
2022-12-05 18:07:01 +0100

    20221105-01

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,14 +3,45 @@
  * see commented examples below
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+import * as bootstrap from 'bootstrap';
+import countdown from 'countdown';
+
 
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+
+
+window.onload = function() {
+    const nextConcertDate = new Date(2023, 0, 8, 19, 00);
+    const nextConcertSite = ''
+
+    const concertCountdown = document.getElementById('concert-countdown');
+
+    
+    
+    setInterval( () => {
+        concertCountdown.textContent = countdown(nextConcertDate)
+            .toString()
+            .replace('months', 'meses')
+            .replace('month', 'mes')
+            .replace('days', 'días')
+            .replace('day', 'día')
+            .replace('hours', 'horas')
+            .replace('hour', 'hora')
+            .replace('minutes', 'minutos')
+            .replace('minute', 'minute')
+            .replace('seconds', 'segundos')
+            .replace('second', 'segundo')
+            .replace('and', 'y');
+
+
+
+    }, 1000);
+    
+    
+}
+ 
+
+
